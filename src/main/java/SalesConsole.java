23387301f32ad88618d30e433920205886351f4b
fprivato <fabrizio.machella@gmail.com>
2015-11-07 00:12:46 +0100

    aumentata precisione del parsing

--- src/main/java/SalesConsole.java
+++ src/main/java/SalesConsole.java
@@ -1,18 +1,34 @@
 import items.Item;
 import items.ItemFactory;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SalesConsole {
 
     private ShoppingBasket basket = new ShoppingBasket();
 
     public void addToBasket(String lineItem) {
-        Integer itemQuantity = Integer.parseInt(lineItem.substring(0,1));
-        Item item = ItemFactory.create(lineItem.substring(2));
+        Matcher m = parseInput(lineItem);
+
+        String stringQuantity = m.group(1);
+        String rest = m.group(2);
+
+        Integer itemQuantity = Integer.parseInt(stringQuantity);
+        Item item = ItemFactory.create(rest);
         basket.addLineItem(itemQuantity,item);
     }
 
     public Receipt calculateReceipt() {
         return basket.checkout();
+    }
+
+
+    private Matcher parseInput(String lineItem) {
+        Pattern p = Pattern.compile("([0-9]+) (.*)");
+        Matcher m = p.matcher(lineItem);
+        m.matches();
+        return m;
     }
 
 }
--- src/test/java/SalesConsoleTest.java
+++ src/test/java/SalesConsoleTest.java
@@ -1,15 +1,18 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SalesConsoleTest {
+    private SalesConsole console;
+
+    @Before
+    public void setUp() throws Exception {
+        console = new SalesConsole();
+    }
 
     @Test
     public void shouldCheckoutInput1Basket(){
-
-        SalesConsole console = new SalesConsole();
-
-
         console.addToBasket("1 book at 12.49");
         console.addToBasket("1 music CD at 14.99");
         console.addToBasket("1 chocolate bar at 0.85");
@@ -21,5 +24,15 @@ public class SalesConsoleTest {
         assertEquals("1 chocolate bar: 0.85",receipt.printItemAt(2));
         assertEquals("Sales Taxes: 1.50",receipt.taxes());
         assertEquals("Total: 29.83",receipt.total());
+    }
+    @Test
+    public void shouldCheckoutInput1ExtraBasket(){
+        console.addToBasket("10 book at 12.49");
+
+        Receipt receipt = console.calculateReceipt();
+
+        assertEquals("10 book: 124.90",receipt.printItemAt(0));
+        assertEquals("Sales Taxes: 0.00",receipt.taxes());
+        assertEquals("Total: 124.90",receipt.total());
     }
 }
